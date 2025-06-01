705a6da515a737f949d086bcbbdb2f07350fe992
pedadapurnalakshmi <pedadapurnalakshmi@gmail.com>
2025-06-02 03:42:26 +0530

    Add files via upload

---
+++ BalanceEquiry.java
@@ -0,0 +1,70 @@
+package bank.management.system;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.ResultSet;
+import javax.swing.*;
+import java.util.*;
+
+class BalanceEquiry extends JFrame implements ActionListener {
+
+    
+    JButton b1;
+    JLabel l1;
+    String pin;
+
+    BalanceEquiry(String pin) {
+        this.pin = pin;
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(760, 1080, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel l3 = new JLabel(i3);
+        l3.setBounds(0, 0, 760, 1080);
+        add(l3);
+
+        l1 = new JLabel();
+        l1.setForeground(Color.WHITE);
+        l1.setFont(new Font("System", Font.BOLD, 16));
+
+        b1 = new JButton("BACK");
+
+        setLayout(null);
+
+        l1.setBounds(130, 350, 400, 35);
+        l3.add(l1);
+
+        b1.setBounds(290, 433, 150, 35);
+        l3.add(b1);
+        int balance = 0;
+        try{
+            Conn c1 = new Conn();
+            ResultSet rs = c1.s.executeQuery("select * from bank where pin = '"+pin+"'");
+            while (rs.next()) {
+                if (rs.getString("type").equals("Deposit")) {
+                    balance += Integer.parseInt(rs.getString("amount"));
+                } else {
+                    balance -= Integer.parseInt(rs.getString("amount"));
+                }
+            }
+        }catch(Exception e){}
+        
+        l1.setText("Your Current Account Balance is Rs "+balance);
+
+        b1.addActionListener(this);
+
+        setSize(760, 1080);
+        
+        setLocation(100, 50);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        setVisible(false);
+        new Transactions(pin).setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new BalanceEquiry("").setVisible(true);
+    }
+}
